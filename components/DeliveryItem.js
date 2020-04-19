e3ac1ddf2fdc7d6054d79dbd4cf6ae2b10e2380f
Sebastian Winkler <sebastian@winkler.dev>
2020-04-19 16:07:37 +0200

    adds delivery list

---
+++ components/DeliveryItem.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import * as firebase from "firebase";
+import {auth, store} from "../firebase";
+import TouchableOpacity from "react-native-web/dist/exports/TouchableOpacity";
+
+export default class DeliveryItem extends Component {
+    constructor(props) {
+        super(props);
+
+        const {id} = props
+
+        this.state = {
+            id: id,
+            data: null,
+            removeBlocked: false,
+        };
+
+        store.collection('deliveries').doc(id).onSnapshot(snapshot => {
+            this.setState({data: snapshot.data()});
+        });
+    }
+
+    handleRemove(_) {
+        this.setState({removeBlocked: true});
+
+        const {id} = this.state
+
+        store.collection('users').doc(auth.currentUser.uid).set(
+            {
+                deliveries: firebase.firestore.FieldValue.arrayRemove(id),
+            },
+            {merge: true}
+        )
+            .then(_ => {
+                this.setState({removeBlocked: false});
+            })
+            .catch(error => {
+                console.error('Error storing delivery:', error)
+            })
+    }
+
+    render() {
+        const {data} = this.state;
+
+        if (!data) {
+            return (<Text>Loading…</Text>);
+        }
+
+        return (
+            <View>
+                <Text style={deliveryItemStyles.idText}>{data.id}</Text>
+                <TouchableOpacity style={deliveryItemStyles.button}
+                                  onPress={e => this.handleRemove(e)}
+                                  disabled={this.state.removeBlocked}>
+                    <Text style={deliveryItemStyles.buttonText}>Remove</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const deliveryItemStyles = StyleSheet.create({
+    idText: {
+        fontSize: 30,
+    },
+    button: {
+        backgroundColor: 'blue',
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    },
+});
--- components/ListDeliveries.js
+++ components/ListDeliveries.js
@@ -1,8 +1,43 @@
-import * as React from 'react';
-import Text from "react-native-web/dist/exports/Text";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {auth, store} from "../firebase";
+import DeliveryItem from "./DeliveryItem";
 
-export default function ListDeliveries(props) {
-  return (
-      <Text>Deliveries</Text>
-  );
+export default class ListDeliveries extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      deliveries: []
+    };
+
+    store.collection('users').doc(auth.currentUser.uid).onSnapshot(snapshot => {
+      this.setState({deliveries: snapshot.data().deliveries});
+    });
+  }
+
+  render() {
+    const {deliveries} = this.state;
+
+    const deliveriesHTML = deliveries.map((item) => {
+      return (
+          <View style={listDeliveriesStyles.delivery} key={item}>
+            <DeliveryItem id={item}/>
+          </View>
+      )
+    })
+
+    return (
+        <View>
+          <Text>Deliveries</Text>
+          {deliveriesHTML}
+        </View>
+    );
+  }
 }
+
+const listDeliveriesStyles = StyleSheet.create({
+  delivery: {
+    marginTop: 15,
+  },
+});
